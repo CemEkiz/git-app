6ba41e45a9faaebb07eff4733b162e291464c15a
Ekiz Cem <ekizcem.pro@gmail.com>
2022-07-19 12:49:23 +0200

    Move Marker on Click

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 class Workout {
 	date = new Date();
 	id = (Date.now() + '').slice(-10);
+	clicks = 0;
 
 	constructor(coords, distance, duration) {
 		this.coords = coords; // [lat, lng]
@@ -17,6 +18,10 @@ class Workout {
 		this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
 			months[this.date.getMonth()]
 		} ${this.date.getDate()}`;
+	}
+
+	click() {
+		this.clicks++;
 	}
 }
 
@@ -81,6 +86,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 class App {
 	#map;
+	#mapZoomLevel = 13;
 	#mapEvent;
 	#workouts = [];
 
@@ -90,6 +96,8 @@ class App {
 		form.addEventListener('submit', this._newWorkout.bind(this));
 
 		inputType.addEventListener('change', this._toggleElevationField);
+
+		containerWorkouts.addEventListener('click', this._movToPopup.bind(this));
 	}
 
 	_getPosition() {
@@ -115,7 +123,7 @@ class App {
 
 		const coords = [latitude, longitude];
 
-		this.#map = L.map('map').setView(coords, 13);
+		this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
 		L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
 			attribution:
@@ -278,6 +286,30 @@ class App {
 		}
 
 		form.insertAdjacentHTML('afterend', html);
+	}
+
+	_movToPopup(e) {
+		const workoutEl = e.target.closest('.workout');
+		// console.log(workoutEl);
+
+		// Guard Clause
+		if (!workoutEl) return;
+
+		const workout = this.#workouts.find(
+			(work) => work.id === workoutEl.dataset.id
+		);
+		// console.log(workout);
+
+		// Leaflet Method (cf. Doc)
+		this.#map.setView(workout.coords, this.#mapZoomLevel, {
+			animate: true,
+			pan: {
+				duration: 1,
+			},
+		});
+
+		// Using the Public Interface
+		workout.click();
 	}
 }
 const work = new Workout();
